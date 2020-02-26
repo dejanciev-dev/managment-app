827892f92a703ec299d8fa4b6b6c40bdc2ba09ca
Dejan Ciev <deni_ciev@yahoo.com>
2020-02-26 01:50:36 +0100

    Add invoice migration

--- src/ManagementApp/ManagementApp.Infrastructure/Migrations/20200226004945_AddInvoice.Designer.cs
+++ src/ManagementApp/ManagementApp.Infrastructure/Migrations/20200226004945_AddInvoice.Designer.cs
@@ -4,14 +4,16 @@ using ManagementApp.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ManagementApp.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200226004945_AddInvoice")]
+    partial class AddInvoice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
