01da07ce58471d81196cbc335e9fda27fafbf872
Dejan Ciev <deni_ciev@yahoo.com>
2020-03-23 16:15:18 +0100

    infrastructure integration test

---
+++ tests/Infrastructure.IntegrationTests/Persistence/ApplicationDbContextTests.cs
@@ -0,0 +1,76 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using ManagementApp.Application.Common.Interfaces;
+using ManagementApp.Domain.Entities;
+using ManagementApp.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Moq;
+using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Infrastructure.IntegrationTests.Persistence
+{
+    public class ApplicationDbContextTests : IDisposable
+    {
+        private readonly string _userId;
+        private readonly DateTime _dateTime;
+        private readonly Mock<IDateTime> _dateTimeMock;
+        private readonly Mock<ICurrentUserService> _currentUserServiceMock;
+
+        private readonly ApplicationDbContext _sut;
+
+        public ApplicationDbContextTests()
+        {
+            _dateTime = new DateTime(2011, 2, 2);
+            _dateTimeMock = new Mock<IDateTime>();
+            _dateTimeMock.Setup(m => m.Now).Returns(_dateTime);
+
+            _userId = "00000000-0000-0000-0000-000000000000";
+            _currentUserServiceMock = new Mock<ICurrentUserService>();
+            _currentUserServiceMock.Setup(m => m.UserId).Returns(_userId);
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+              .UseInMemoryDatabase(Guid.NewGuid().ToString())
+              .Options;
+
+            var operationalStoreOptions = Options.Create(
+                new OperationalStoreOptions
+                {
+                    DeviceFlowCodes = new TableConfiguration("DeviceCodes"),
+                    PersistedGrants = new TableConfiguration("PersistedGrants")
+                });
+
+            _sut = new ApplicationDbContext(options, operationalStoreOptions, _currentUserServiceMock.Object, _dateTimeMock.Object);
+
+            _sut.Invoices.Add(new Invoice
+            {
+                Id = 1
+            });
+
+            _sut.SaveChanges();
+        }
+
+        [Fact]
+        public async Task SaveChangesAsync_GivenNewInvoice_ShouldSetCreatedProperties()
+        {
+            var item = new Invoice
+            {
+                Id = 2
+            };
+
+            _sut.Invoices.Add(item);
+
+            await _sut.SaveChangesAsync();
+
+            item.Created.ShouldBe(_dateTime);
+            item.CreatedBy.ShouldBe(_userId);
+        }
+
+        public void Dispose()
+        {
+            _sut?.Dispose();
+        }
+    }
+}
