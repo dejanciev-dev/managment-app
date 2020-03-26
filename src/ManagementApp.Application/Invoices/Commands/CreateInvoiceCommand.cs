93dd568481fa20a40a53c29bcfc16e8009876a4d
Dejan Ciev <deni_ciev@yahoo.com>
2020-03-26 22:35:55 +0100

    Change command and query structure

--- src/ManagementApp.Application/Invoices/Commands/CreateInvoiceCommand.cs
+++ src/ManagementApp.Application/Invoices/Commands/CreateInvoiceCommand.cs
@@ -1,8 +1,13 @@
-﻿using ManagementApp.Application.Invoices.ViewModels;
+﻿using AutoMapper;
+using ManagementApp.Application.Common.Interfaces;
+using ManagementApp.Application.Invoices.ViewModels;
+using ManagementApp.Domain.Entities;
 using ManagementApp.Domain.Enums;
 using MediatR;
 using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ManagementApp.Application.Invoices.Commands
 {
@@ -26,5 +31,26 @@ namespace ManagementApp.Application.Invoices.Commands
         public TaxType TaxType { get; set; }
         public double AmountPaid { get; set; }
         public IList<InvoiceItemVm> InvoiceItems { get; set; }
+
+        public class CreateInvoiceCommandHandler : IRequestHandler<CreateInvoiceCommand, int>
+        {
+            private readonly IApplicationDbContext _context;
+            private readonly IMapper _mapper;
+
+            public CreateInvoiceCommandHandler(IApplicationDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<int> Handle(CreateInvoiceCommand request, CancellationToken cancellationToken)
+            {
+                var entity = _mapper.Map<Invoice>(request);
+
+                _context.Invoices.Add(entity);
+                await _context.SaveChangesAsync(cancellationToken);
+                return entity.Id;
+            }
+        }
     }
 }
